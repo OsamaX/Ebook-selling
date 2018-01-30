7bdaf2842417a5a0f4dae2b9dd941db42244d38c
Osama AVvan <gamingfreaks786@gmail.com>
2018-01-30 12:27:57 +0500

    your comment

---
+++ utilities/imgValidate.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = function imgValidate(profileImg, loc, next, res, cb) {
+
+    if (profileImg) {
+        if (profileImg.mimetype != 'image/png' && profileImg.mimetype != 'image/jpeg') {
+            return res.send("Invalid Image Type");
+        } else {
+
+            let filePath = path.join(process.cwd(), 'uploads', loc, '/', new Date().getMilliseconds().toString()) + profileImg.name;
+
+            profileImg.mv(filePath, err => {   
+                if (err) console.log(err);
+
+                filePath = filePath.split("\\");
+                filePath = filePath.splice(6);
+                filePath = path.join(filePath[0], filePath[1]);
+               
+                cb(filePath);
+            });
+        }
+
+} else {
+    return res.send("Select an image");
+}
+
+}
